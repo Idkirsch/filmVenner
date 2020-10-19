8c8dc213a962d4987b8ddcbfb2c47779df30f5ec
Idkirsch <s195483@student.dtu.dk>
2020-10-19 21:55:14 +0200

    Login skærm på vej + farveskala

--- app/src/main/java/com/example/filmvenner/MainActivity.java
+++ app/src/main/java/com/example/filmvenner/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.filmvenner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button videre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        videre = (Button) findViewById(R.id.buttonVidere);
+        videre.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == videre){
+            Intent login = new Intent(this, Login.class);
+            this.startActivity(login);
+        }
+
     }
 }
