d5c4d14b0bb60002ac3c1c4357eebbcf204e6759
Idkirsch <s195483@student.dtu.dk>
2020-10-22 15:57:20 +0200

    Lidt mere kommentarer

--- app/src/main/java/com/example/filmvenner/MainActivity.java
+++ app/src/main/java/com/example/filmvenner/MainActivity.java
@@ -10,8 +10,16 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     /*
+     Her i denne blok kan vi skrive vigtig info til hinanden.
+
     Jeg har lavet en meget simpel main activity, hvor der bare er én knap som hedder "videre".
     Nede i onClick metoden kan man ændre hvilken aktivitet, knappen skal tage dig hen til.
+
+    Tjek lige den her fil ud: app > res > values > colors.xml
+    Her kan man gemme sine egne farver under et navn, og bruge de farver i sit layout.
+    Hvis alle gør dét, bliver det rigtig nemt og hurtigt at ændre farveskalaen til sidst ;-)
+    Jeg har lavet et par stykker, og I skal være velkomne til at ændre dem eller ændre navngivningen
+
      */
 
 
