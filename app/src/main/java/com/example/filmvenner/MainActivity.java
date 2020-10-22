9e86ac3192db3962f6c16396efe9dcb0e6898fce
Idkirsch <s195483@student.dtu.dk>
2020-10-22 15:53:49 +0200

    Har lige foklaret hvad knappen "videre" gør

--- app/src/main/java/com/example/filmvenner/MainActivity.java
+++ app/src/main/java/com/example/filmvenner/MainActivity.java
@@ -9,6 +9,12 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    /*
+    Jeg har lavet en meget simpel main activity, hvor der bare er én knap som hedder "videre".
+    Nede i onClick metoden kan man ændre hvilken aktivitet, knappen skal tage dig hen til.
+     */
+
+
     Button videre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         if(view == videre){
+            // her kan man ændre hvilken side knappen tager dig hen til
             Intent login = new Intent(this, Login.class);
             this.startActivity(login);
         }
