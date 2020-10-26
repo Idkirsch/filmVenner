7baf9da748ba4f42fbbda69f94f978fe534d0b28
Zahra <s170422@student.dtu.dk>
2020-10-26 09:15:00 +0100

    carousel fejl

--- app/src/main/java/com/example/filmvenner/MainActivity.java
+++ app/src/main/java/com/example/filmvenner/MainActivity.java
@@ -11,18 +11,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     /*
      Her i denne blok kan vi skrive vigtig info til hinanden.
-
     Jeg har lavet en meget simpel main activity, hvor der bare er én knap som hedder "videre".
     Nede i onClick metoden kan man ændre hvilken aktivitet, knappen skal tage dig hen til.
-
     Tjek lige den her fil ud: app > res > values > colors.xml
     Her kan man gemme sine egne farver under et navn, og bruge de farver i sit layout.
     Hvis alle gør dét, bliver det rigtig nemt og hurtigt at ændre farveskalaen til sidst ;-)
     Jeg har lavet et par stykker, og I skal være velkomne til at ændre dem eller ændre navngivningen
-
      */
-
-
     Button videre;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
