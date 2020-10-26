1e26f3617f873840af0aba392b142d0d27010ef6
Idkirsch <s195483@student.dtu.dk>
2020-10-26 09:26:24 +0100

    Bottom navigation bare on the way. It reacts to clicks, but it cannot change activity.

--- app/src/main/java/com/example/filmvenner/MainActivity.java
+++ app/src/main/java/com/example/filmvenner/MainActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
     Button videre;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,16 +39,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         videre = (Button) findViewById(R.id.buttonVidere);
         videre.setOnClickListener(this);
 
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
     }
 
     @Override
     public void onClick(View view) {
         if(view == videre){
-            // her kan man ændre hvilken side knappen tager dig hen til
-            Intent login = new Intent(this, Login.class);
-            this.startActivity(login);
+            Intent videre = new Intent(this, Login.class);
+            this.startActivity(videre);
         }
-
+        //Intent navHome = new Intent(this, WordpressActivity.class);
+        //  this.startActivity(navHome);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener
+            navListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item)
+        {
+            // By using switch we can easily get
+            // the selected fragment
+            // by using there id.
+            switch (item.getItemId()) {
+                case R.id.home:
+                 //   Intent videre = new Intent(this, Home.class);
+                  //  this.startActivity(home);
+                    System.out.println("pressed home");
+                    break;
+                case R.id.search:
+                    System.out.println("pressed searhc");
+                    break;
+                case R.id.profile:
+                    System.out.println("pressed profile");
+                    break;
+            }
+            return true;
+        }
+    };
 }
