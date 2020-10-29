14bc4fd0a1cf6d72c252756513b3eca71bd1c0c6
Christine <s141361@student.dtu.dk>
2020-10-29 02:05:02 +0100

    Rated button aktivitet

--- app/src/main/java/com/example/filmvenner/ProfileFragment.java
+++ app/src/main/java/com/example/filmvenner/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.filmvenner;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +25,8 @@ public class ProfileFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    Button b1;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -59,6 +63,15 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        Button b1 = (Button )view.findViewById(R.id.ratedbutton);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getActivity(),ProfileRated.class));
+            }
+        });
+        return view;
     }
 }
