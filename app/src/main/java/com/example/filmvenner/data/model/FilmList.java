e1f9a4e495e195861746715d1e432bf19a78f03c
Zahra <s170422@student.dtu.dk>
2020-11-06 17:49:02 +0100

    FilmList added

---
+++ app/src/main/java/com/example/filmvenner/data/model/FilmList.java
@@ -0,0 +1,17 @@
+package com.example.filmvenner.data.model;
+
+public class FilmList {
+    private int image;
+
+    public FilmList(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
