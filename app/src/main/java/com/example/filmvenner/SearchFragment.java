00ef2f0ad4b1bcf1b43bc0e5069c65021bd03a42
Zahra <s170422@student.dtu.dk>
2020-11-06 18:06:50 +0100

    RecyclerViewAdapter added

--- app/src/main/java/com/example/filmvenner/SearchFragment.java
+++ app/src/main/java/com/example/filmvenner/SearchFragment.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.filmvenner.data.model.FilmList;
+import com.example.filmvenner.ui.login.RecyclerViewAdapter;
 
 import java.util.ArrayList;
 import java.util.List;
