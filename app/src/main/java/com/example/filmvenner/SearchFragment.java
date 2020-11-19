4eb681115c10a9cc68c7ad0b35bd6ccdbc3dc4bc
Idkirsch <s195483@student.dtu.dk>
2020-11-19 12:00:18 +0100

    Search title responds with json object in log

--- app/src/main/java/com/example/filmvenner/SearchFragment.java
+++ app/src/main/java/com/example/filmvenner/SearchFragment.java
@@ -31,6 +31,8 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     private RecyclerView recyclerView1, recyclerView2, recyclerView3;
     private Button searchButton;
     private EditText searchField;
+    String url ="https://www.omdbapi.com/?t=";
+    String apikey ="&apikey=3e1a983d";
 
 
     @Override
@@ -81,21 +83,22 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         System.out.println("clicked search");
         System.out.println(searchField.getText().toString());
-        //"http://www.omdbapi.com/?s=inception&apikey=[yourkey]
         RequestQueue queue = Volley.newRequestQueue(getContext());
-        //String url ="https://www.google.com";
-       // String url ="https://www.omdbapi.com/?s=inception&apikey=[3e1a983d]";
-        String url ="https://www.omdbapi.com/?t=avengers&apikey=3e1a983d";
+        String title = searchField.getText().toString();
 
+
+        String request = url+title+apikey;
 
         // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, request,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         // Display the first 500 characters of the response string.
                         //searchField.setText("Response is: "+ response.substring(0,500));
+
                         System.out.println("Response is: "+ response.substring(0,500));
+
                     }
                 }, new Response.ErrorListener() {
             @Override
@@ -105,7 +108,7 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
             }
         });
 
-// Add the request to the RequestQueue.
+        // Add the request to the RequestQueue.
         queue.add(stringRequest);
 
         //https://developer.android.com/training/volley/simple#java
@@ -113,59 +116,3 @@ public class SearchFragment extends Fragment implements View.OnClickListener {
     }
 }
 
-
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link SearchFragment#newInstance} factory method to
- * create an instance of this fragment.
- ////
-public class SearchFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public SearchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SearchFragment.
-
-    // TODO: Rename and change types and number of parameters
-    public static SearchFragment newInstance(String param1, String param2) {
-        SearchFragment fragment = new SearchFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
-    }
-}
- */
