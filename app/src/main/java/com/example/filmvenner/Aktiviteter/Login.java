1ea0250ad909f1071042c1b5d62e7e91995bd205
Idkirsch <s195483@student.dtu.dk>
2021-01-06 14:05:23 +0100

    Når man logger ind tjekkes der om brugernavnet findes i databasen

--- app/src/main/java/com/example/filmvenner/Aktiviteter/Login.java
+++ app/src/main/java/com/example/filmvenner/Aktiviteter/Login.java
@@ -47,34 +47,38 @@ public class Login extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View view) {
         if(view == login){
             String inputUsername = username.getText().toString();
+            System.out.println("input username is: "+inputUsername);
             String inputPassword = password.getText().toString();
 
 
-            //check if name MARIE is in document TEST in collection users.
+            //check if name MARIE is in document "username"  in collection users.
             // if it is, print it in console
 
-//
-//            DocumentReference docRef = database.collection("users").document(inputUsername);
-//            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//                @Override
-//                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                    if(task.isSuccessful()){
-//                        DocumentSnapshot document = task.getResult();
-//                        if(document.exists()){
-//                            if(document.getString("name").equals(inputUsername)){
-//                                System.out.println("the username equals the username from database");
-//                                System.out.println("name from database= "+document.get("name"));
-//                                System.out.println("name from inputfield= "+inputUsername);
-//                            }
-//
-//                        }else{
-//                            System.out.println("no such document");
-//                        }
-//                    }else{
-//                        System.out.println("get failed with "+task.getException());
-//                    }
-//                }
-//            });
+
+            //DocumentReference docRef = database.collection("users").document(inputUsername);
+
+            DocumentReference docRef = database.collection("users").document("BrugernavnET");
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if(task.isSuccessful()){
+                        DocumentSnapshot document = task.getResult();
+                        if(document.exists()){
+                            if(document.getString("username").equals(inputUsername)){
+                                System.out.println("the username equals the username from database");
+                                System.out.println("name from database= "+document.get("username"));
+                                System.out.println("name from inputfield= "+inputUsername);
+                            }
+
+                        }else{
+                            System.out.println("no such document");
+                        }
+                    }else{
+                        System.out.println("get failed with "+task.getException());
+                    }
+                }
+            });
+
 
             if(inputUsername.equals(expected_username) && inputPassword.equals(expected_password)){ // checks if input is equal to expected. TODO: get expected password from database
                 System.out.println("yess man ");
