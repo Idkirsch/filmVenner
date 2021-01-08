eec9f3d30ea30169f89e774e041345111701fb21
Zahra <s170422@student.dtu.dk>
2021-01-08 12:55:05 +0100

    ændringer i RateFilmActivity

--- app/src/main/java/com/example/filmvenner/RateFilmActivity.java
+++ app/src/main/java/com/example/filmvenner/RateFilmActivity.java
@@ -5,10 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RateFilmActivity extends AppCompatActivity {
+    private String user, movie_title;
+    private int rate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rate_film);
     }
+
+    public RateFilmActivity(String user, int rate, String movie_title) {
+        this.user = user;
+        this.rate = rate;
+        this.movie_title = movie_title;
+    }
+
+    public String getUser(){ return user;}
+    public String getMovie_title(){ return movie_title;}
+    public int getRate(){ return rate;}
 }
