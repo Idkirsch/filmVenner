61d4916c4029f6ba7640e41c9925110d4b9c589a
Zahra <s170422@student.dtu.dk>
2021-01-08 11:12:12 +0100

    layout til rate a film added

---
+++ app/src/main/java/com/example/filmvenner/RateFilmActivity.java
@@ -0,0 +1,14 @@
+package com.example.filmvenner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RateFilmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_film);
+    }
+}
