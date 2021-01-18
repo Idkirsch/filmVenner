978ab3c7dd74185834a5c1ba5c468b5a8b646d9e
Idkirsch <s195483@student.dtu.dk>
2021-01-18 15:55:01 +0100

    kan tilføje et review til databasen

--- app/src/main/java/com/example/filmvenner/Aktiviteter/skalSlettes.java
+++ app/src/main/java/com/example/filmvenner/Aktiviteter/skalSlettes.java
@@ -4,17 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.example.filmvenner.R;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Map;
+
 public class skalSlettes extends AppCompatActivity {
     String MovieID = "372058";
+    TextView reviewView;
 
     FirebaseFirestore database = FirebaseFirestore.getInstance();
     DocumentReference docRef = database.collection("Film").document(MovieID);
@@ -27,6 +34,14 @@ public class skalSlettes extends AppCompatActivity {
         setContentView(R.layout.activity_skal_slettes);
 
 
+        reviewView = findViewById(R.id.reviewFromDB);
+
+        addReview();
+
+    }
+
+
+    public void retrievereviews() {
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
@@ -34,33 +49,69 @@ public class skalSlettes extends AppCompatActivity {
                     DocumentSnapshot documentSnapshot = task.getResult();
                     System.out.println("DocumentSnapshot data: " + documentSnapshot.getData());
 
+                    Map<String, Object> map = documentSnapshot.getData();
+                    for (Map.Entry<String, Object> entry : map.entrySet()) {
+//                        System.out.println("entrykey: " + entry.getKey());
+//                        System.out.println("entryvalue: " + entry.getValue());
+                        ArrayList<String> reviewsRatings = (ArrayList<String>) entry.getValue();
+//                        System.out.println("reviwsratings : " + reviewsRatings);
+                        String review = reviewsRatings.get(0);
+                        String rating = reviewsRatings.get(1);
+                        System.out.println("review: " + review);
+                        System.out.println("rating: " + rating);
+                        //add to recyclerview
+                    }
                 }
             }
         });
     }
+
+    public void addReview() {
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot documentSnapshot = task.getResult();
+                    System.out.println("DocumentSnapshot data: " + documentSnapshot.getData());
+                    ArrayList<String> currentReviewsRatings = new ArrayList<>();
+                    ArrayList<String> newReviewsRatings = new ArrayList<>();
+
+                    String newreview = "test her er et review3";
+                    String currentReview = new String();
+                    String currentRating = new String();
+
+
+                    Map<String, Object> map = documentSnapshot.getData();
+
+                    for (Map.Entry<String, Object> entry : map.entrySet()) {
+                        currentReviewsRatings = (ArrayList<String>) entry.getValue();
+
+                        currentReview = currentReviewsRatings.get(0);
+                        currentRating = currentReviewsRatings.get(1);
+
+
+                    }
+
+                    newReviewsRatings.add(newreview);
+                    newReviewsRatings.add(currentRating);
+
+
+                    map.put("Pis", newReviewsRatings);
+                    System.out.println("map pippi: " + map);
+
+                    docRef.set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            System.out.println("review succesfully altered");
+                        }
+                    });
+
+                }
+            }
+        });
+    }
+
+
 }
 
 
-// docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//@Override
-//public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//        if(task.isSuccessful()){
-//        DocumentSnapshot document = task.getResult();
-//        if(document.exists()){
-//        System.out.println("DocumentSnapshot data: "+ document.getData());
-//        System.out.println("document get name: "+ document.get("name"));
-//        name = document.getString("name");
-//
-//        //create map to save data in
-//        //Map<String, Object> user = document.getData();
-//
-//        }else{
-//        System.out.println("no such document");
-//        }
-//        }else{
-//        System.out.println("get failed with "+task.getException());
-//        }
-//        }
-//        });
-//
-//        }
