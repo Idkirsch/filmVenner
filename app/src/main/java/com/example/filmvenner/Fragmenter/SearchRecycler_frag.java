b754136667e247229189de6e193a0175a97fc8b9
Marie <S185363@student.dtu.dk>
2021-01-19 11:39:12 +0100

    tilfføjet 3. karrussel

--- app/src/main/java/com/example/filmvenner/Fragmenter/SearchRecycler_frag.java
+++ app/src/main/java/com/example/filmvenner/Fragmenter/SearchRecycler_frag.java
@@ -47,6 +47,7 @@ public class SearchRecycler_frag extends Fragment{
     ArrayList<Movie> movies;
     ArrayList<FilmList> exampleList = new ArrayList<>();
     ArrayList<FilmList> exampleList2 = new ArrayList<>();
+    ArrayList<FilmList> exampleList3 = new ArrayList<>();
     private String prefixImage = "https://image.tmdb.org/t/p/w500";
 
     LinearLayoutManager layoutManager1 = new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
@@ -155,6 +156,7 @@ public class SearchRecycler_frag extends Fragment{
     public void callAPI() {
         String requestPopular = "https://api.themoviedb.org/3/movie/popular?api_key=fa302bdb2e93149bd69faa350c178b38&language=en-US&page=1";
         String requestTopRated = "https://api.themoviedb.org/3/movie/top_rated?api_key=fa302bdb2e93149bd69faa350c178b38&language=en-US&page=1";
+        String requestTopDk = "https://api.themoviedb.org/3/movie/top_rated?api_key=fa302bdb2e93149bd69faa350c178b38&language=en-US&page=1&region=DK";
 
 
         System.out.println("in Call API method, Search recyc frag");
@@ -224,6 +226,39 @@ public class SearchRecycler_frag extends Fragment{
                     recyclerView2.setLayoutManager(layoutManager2);
                     RecyclerViewAdapter adapter2 = new RecyclerViewAdapter(exampleList2, getActivity());
                     recyclerView2.setAdapter(adapter2);
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, error -> System.out.println(" couldnt't get answer from API in search recycler frag or couldnt populate views"));
+        mRequestQueue.add(jsonObjectRequest);
+
+        jsonObjectRequest = new JsonObjectRequest(
+                Request.Method.GET, requestTopDk, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+//                System.out.println("in onResponse");
+                try {
+                    JSONObject movieJson = response;
+                    JSONArray moviesJson = movieJson.getJSONArray("results");
+
+                    movies = Movie.fromJson(moviesJson);
+
+                    for (int i = 0; i < moviesJson.length(); i++) {
+                        String imagePath = movies.get(i).getmImageResource().toString();
+                        String fullImagePath = prefixImage + imagePath;
+//                        System.out.println("full image path: " + fullImagePath);
+                        //FilmList item = new FilmList(imagePath);
+                        FilmList item = new FilmList(fullImagePath,"","");
+
+                        exampleList3.add(item);
+                    }
+
+                    recyclerView3.setLayoutManager(layoutManager3);
+                    RecyclerViewAdapter adapter3 = new RecyclerViewAdapter(exampleList3, getActivity());
+                    recyclerView3.setAdapter(adapter3);
 
 
                 } catch (JSONException e) {
